7ce2a9e2a157c9649878db996625b7f176f3acfa
aditya4454 <53806120+aditya4454@users.noreply.github.com>
2022-04-30 13:39:26 +0530

    Update forms.py

--- recognition/forms.py
+++ recognition/forms.py
@@ -13,7 +13,7 @@ class DateForm(forms.Form):
 
 
 class UsernameAndDateForm(forms.Form):
-	username=forms.CharField(max_length=30)
+			username=forms.CharField(max_length=30)
 	date_from=forms.DateField(widget = forms.SelectDateWidget(empty_label=("Choose Year", "Choose Month", "Choose Day")))
 	date_to=forms.DateField(widget = forms.SelectDateWidget(empty_label=("Choose Year", "Choose Month", "Choose Day")))
 
